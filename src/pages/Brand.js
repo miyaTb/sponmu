c981455389a4c53a342aae8bd75562fd42b28698
Akira Miyamoto <64848070+miyaTb@users.noreply.github.com>
2025-04-01 20:48:11 +0900

    add/ルーティング
    トップ/ログイン/商品/商品一覧/ブランド/コラム/エラー
    各ページのアドレス設定実装

---
+++ src/pages/Brand.js
@@ -0,0 +1,9 @@
+function Brand() {
+    return (
+    <div>
+        <h1>Brand</h1>
+    </div>
+    );
+}
+
+export default Brand;
---
+++ src/pages/Column.js
@@ -0,0 +1,9 @@
+function Column() {
+    return (
+    <div>
+        <h1>Column</h1>
+    </div>
+    );
+}
+
+export default Column;
