9f583d785e90838de48409cc8f11533230e9a1db
utan1130 <yuki.oka.1130@gmail.com>
2025-05-10 18:35:10 +0900

    一旦保存

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import product from './pages/product.jsx';
 
 function App() {
   return (
     <>
       <Header />
+      <product />
       <Footer />
     </>
   );
