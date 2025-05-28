2610408786178e5edcd3ade56a3cc902bbed1358
utan1130 <yuki.oka.1130@gmail.com>
2025-05-28 23:36:06 +0900

    header一部修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
         <h1><img src={headerLogo} alt="sponmu" /></h1>
         <ul className="headerUl">
           <li><a href="/products">商品一覧</a></li>
-          <li><a href="#">レシピ</a></li> 
+          <li><a href="/recipes">レシピ</a></li> 
           <li><a href="#"><img src={shopIcon} alt="かご" /></a></li> 
         </ul>
       </div>
