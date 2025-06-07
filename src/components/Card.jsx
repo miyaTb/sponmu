c652df9d75fe4919fa069e2ccc5a6246cfece780
todorihayato <hayabrother1416@gmail.com>
2025-06-07 10:50:10 +0900

    add/カードコンポーネント作成

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "./MyComponent.module.css";
 
-const Card = ({ image, title, description }) => {
+const Card = ({ image, title, time, kcal }) => {
   return (
     <div className={styles.card}>
       <img className={styles.cardImage} src={image} alt={title} />
       <div className={styles.cardContent}>
         <h2 className={styles.cardTitle}>{title}</h2>
-        <p className={styles.cardDescription}>{description}</p>
+        <div className={styles.cardDescription}>
+          <p className={styles.cardTime}>{time + "分"}</p>
+          <p className={styles.cardKcal}>{kcal + "KCal"}</p>
+        </div>
       </div>
     </div>
   );
