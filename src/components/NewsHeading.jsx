566dba2a319776ec7f9d07ee2732fb4d595996cb
todorihayato <hayabrother1416@gmail.com>
2025-06-29 18:55:46 +0900

    add/Newsページ作成

--- src/components/NewsHeading.jsx
+++ src/components/NewsHeading.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./MyComponent.module.css";
 
 const NewsHeading = ({ date, title = "お知らせ" }) => {
@@ -8,6 +9,12 @@ const NewsHeading = ({ date, title = "お知らせ" }) => {
       <h2 className={styles.newsTitle}>{title}</h2>
     </div>
   );
+};
+
+NewsHeading.propTypes = {
+  date: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  button: PropTypes.bool,
 };
 
 export default NewsHeading;
