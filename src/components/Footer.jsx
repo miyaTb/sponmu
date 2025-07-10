1e644be1ca923e6139ce8b1ad377166743d3f75f
utan1130 <yuki.oka.1130@gmail.com>
2025-07-10 19:46:25 +0900

    footerテキスト・画像の追加

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import footerLogo from '../assets/footerLogo.png';
+import X from '../assets/X.svg';
+import Instagram from '../assets/Instagram.svg';
+
 
 export default function footer() {
   return (
     <footer>
         <div className='footerText'>
             <img src={footerLogo} alt="sponmu" />
-            <p>〒000-0000<br/>
-            aaaaaaaaaaa
-            </p>
+            <div className='footerDetail'>
+              <span>〒049-3504</span>
+              <span>北海道山越郡長万部町美畑39 スポンジ農場</span>
+              <span>Tel. 090-3939-6403</span>
+            </div>
         </div>
         <div className='footerIcon'>
-          <img src="" alt="Instagram" />
-          <img src="" alt="facebook" />
+          <img src={X} alt="X" />
+          <img src={Instagram} alt="Instagram" />
         </div>
         <p className='footerSponmu'>@スポンジーズ</p>
     </footer>
