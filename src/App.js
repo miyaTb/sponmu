8279fac24a08a03526266fd60ac9d42e5575e877
Akira Miyamoto <64848070+miyaTb@users.noreply.github.com>
2025-08-07 23:58:01 +0900

    Feature/miya/cart (#8)
    * add:cartページの骨組み
    
    * add:カート画面から注文完了まで
    
    * fix:ヘッダー内カゴボタンを正式なものへ

--- src/App.js
+++ src/App.js
@@ -1,37 +1,48 @@
-import "./App.css";
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import ProductList from './pages/ProductList';
+import Product from './pages/Product';
+import Brand from './pages/Brand';
+import Column from './pages/Column';
+import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
+import CartForm from './pages/CartForm';
+import OrderComplete from './pages/OrderComplete';
+import ProductLayout from './pages/ProductLayout';
 import "./styles/variables.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Product from "./pages/Product";
-import Brand from "./pages/Brand";
-import Column from "./pages/Column";
-import NotFound from "./pages/NotFound";
-import ProductLayout from "./pages/ProductLayout";
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<ProductLayout />}>
-            <Route index element={<ProductList />} />
-            <Route path=":id" element={<Product />} />
-          </Route>
-          <Route path="/brand" element={<Brand />} />
-          <Route path="/column" element={<Column />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-      <p>sponmu</p>
-      <Footer />
-    </div>
+    <CartProvider>
+      <div className="App">
+        <Header />
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<ProductLayout />}>
+              <Route index element={<ProductList />} />
+              <Route path=":id" element={<Product />} />
+            </Route>
+            <Route path="/brand" element={<Brand />} />
+            <Route path="/column" element={<Column />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/cart/form" element={<CartForm />} />
+            <Route path="/order/complete" element={<OrderComplete />} />
+            {/* 想定外アドレスは NotFound ページへ */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+        <p>sponmu</p>
+        <Footer />
+      </div>
+    </CartProvider>
   );
 }
 
